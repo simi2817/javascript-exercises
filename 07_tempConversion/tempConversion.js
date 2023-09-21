b008a3d4a1fb7ef31f6a4ee561ff42414c901d9c
simi2817 <sreenivass@TM4112.local>
2023-09-21 10:29:10 +0100

    solved 01_helloWorld till 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  const celsius = parseFloat(((temperature - 32) * 5 / 9).toFixed(1));
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  const fahrenheit = parseFloat(((temperature / 5) * 9 + 32).toFixed(1));
+  return fahrenheit;
 };
 
 // Do not edit below this line
