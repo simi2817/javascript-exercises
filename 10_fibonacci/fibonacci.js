d34698969ce9763cab05a1a136f4746effb42a03
simi2817 <sreenivass@TM4112.local>
2023-09-28 17:54:57 +0100

    solved 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    let n1 = 0;
+    let n2 = 1;
+    let fibonacciSum = 0;
+    if(index < 0)   return 'OOPS';
+    for(let i = 0; i < index; i++) {
+        n1 = n2;
+        n2 = fibonacciSum;
+        fibonacciSum = n1 + n2;
+    }
+
+    return fibonacciSum;
 
 };
 
