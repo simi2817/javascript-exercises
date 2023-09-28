41852ff6a55f0d2f58c60d3d8648d1e6b60cffc4
simi2817 <sreenivass@TM4112.local>
2023-09-28 17:16:21 +0100

    attempted 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (inputStr) {
+    const onlyString = inputStr.toLowerCase().split('').filter(str => str.match(/[A-Za-z]/)).join('');
+    const reversedString = onlyString.split('').reverse().join('');
+    if(reversedString === onlyString)
+        return true;
+    else
+        return false;
 };
 
 // Do not edit below this line
